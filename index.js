9bb0683f2a48df02baffa0f1449f9b8b6034fa6b
Sfzmango <maunghtike1999@gmail.com>
2022-04-21 01:43:25 -0700

    WIP github license API+WIP array input on readme

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const inquirer = require('inquirer');
+const githubApi = 'https://api.github.com/';
 
 const licenses = ["Public Domain & Equivalents", "Permissive License", "Copyleft", "Noncommercial License", "Proprietary License", "Confidential/Classified"];
+
+const techUsed = ["HTML", "CSS", "Javascript", "Node.js", "VS Code", "Sublime", "Git/Github", "Chrome Developer Tools"];
 
 inquirer
     .prompt([
@@ -38,18 +41,14 @@ inquirer
         },
         {
             name: 'techologies',
-            type: 'input',
+            type: 'checkbox',
             message: 'Please enter technologies used:',
+            choices: techUsed,
         },
         {
-            name: 'siteGif',
+            name: 'demoGif',
             type: 'input',
-            message: 'Please enter the relative directory of the demo gif:',
-        },
-        {
-            name: 'credits',
-            type: 'input',
-            message: 'Please enter credits:',
+            message: 'Please enter the directory of the demo gif relative to index.js:',
         },
         {
             name: 'test',
@@ -72,65 +71,83 @@ inquirer
             type: 'input',
             message: "Please enter the repository's URL",
         },
+        {
+            name: 'credits',
+            type: 'input',
+            message: 'Please enter people to credit followed by their URL:',
+        },
 
     ])
     .then((response) => {
-        console.log(response)
-        fs.writeFile(`${response.title}.md`, `#${response.title}
-### By ${response.username}
-### Email: ${response.email}
+        console.log(response);
 
+
+
+
+
+        fs.writeFile(`${response.title}.md`, `# ${response.title}
+<br><br>
 
 ## <ins> Table of Contents: </ins>
 
-    - [Description](#-description-)
-    - [Installation](#-installation-)
-    - [Usage](#-usage-)
-    - [Technologies and Programs Used](#-technologies-and-programs-used-)
-    - [Site Demonstration](#-site-demonstration-)
-    - [Links](#-links-)
-    - [Credits](#-credits-)
-    - [License](#-license-)
-        
+- [Questions](#-questions-)
+- [Description](#-description-)
+- [Installation](#-installation-)
+- [Usage](#-usage-)
+- [Technologies and Programs Used](#-technologies-and-programs-used-)
+- [Demonstration](#-demonstration-)
+- [Links](#-links-)
+- [Credits](#-credits-)
+- [License](#-license-)
+<br><br>  
+
+## <ins> Questions? </ins>
+
+### By [${response.username}](https://github.com/${response.username})
+### Email: ${response.email}
+<br><br>
 
 ## <ins> Description: </ins>
         
 ${response.description}
-        
+<br><br>      
 
 ## <ins> Installation: </ins>
         
 ${response.install}
-
+<br><br>
 
 ## <ins> Usage: </ins>
         
 ${response.usage}
-        
+<br><br>    
 
 ## <ins> Technologies and Programs Used: </ins>
         
 ${response.technologies}
-    
+<br><br> 
         
-## <ins> Site Demonstration: </ins>
+## <ins> Demonstration: </ins>
         
-![Site Demonstration](${response.siteGif})
-        
+![Demonstration](${response.demoGif})
+<br><br>   
 
 ## <ins> Links: </ins>
         
 - [Github Deployed Website](${response.deployWeb})
 - [Github Repository](${response.deployRepo})
-        
+<br><br>     
 
 ## <ins> Credits: </ins>
 
+Special Thanks to: 
 ${response.credits}
-
+<br><br>
 
 ## <ins> License: </ins>
         
+Copyright (c) 2022 ${response.username}
+<br><br>
 ${response.license}
 `, (err) =>
             err ? console.error(err) : console.log("Created README!"))
