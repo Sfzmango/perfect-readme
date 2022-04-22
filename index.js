09914ac2e90205b6995b60149ff2aea8c956b54b
Sfzmango <maunghtike1999@gmail.com>
2022-04-21 21:54:29 -0700

    add comments on index.js to annotate notes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// allows usage of node package functions. we use import over require for compatibility purposes with node fetch
 import fs from "fs";
 import inquirer from 'inquirer';
 import fetch from 'node-fetch';
 
+// arrays for license + badge and technology used prompts
 const licenseArr = ["cc0-1.0", "mit", "mpl-2.0", "apache-2.0", "gpl-3.0", "agpl-3.0"]
 const badgeArr = ["![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)", "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)", "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)", "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)", "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)", "![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)",];
-
-
 const techUsed = ["- HTML", "- CSS", "- Javascript", "- Third-Party APIs", "- Node.js", "- VS Code", "- Sublime Text", "- Git/Github", "- Chrome Developer Tools"];
 
+// pulls up prompts for the user and uses those responses in the .then promise
 inquirer
     .prompt([
         {
@@ -82,17 +83,29 @@ inquirer
     .then((response) => {
         console.log(response);
 
+        // use github api for copy and pasting the selected license license 
         const githubApi = 'https://api.github.com/licenses/';
 
+        // function to organize the response data and create the readme file
+        // we use async and await in order to be able to use the fetch function in node
         var genReadme = async () => {
             console.log(`${githubApi}${response.license}`);
+
+            // fetches the github license information and saves it as consts
             const apiResponse = await fetch(`${githubApi}${response.license}`);
             const apiData = await apiResponse.json();
+
             console.log(apiData);
             console.log(apiData);
             console.log(response.technologies);
+
+            // formats technologies used response in order to format nicely on markdown
             let newTechArr = response.technologies.join('<br>\r\n');
+
             console.log("new tech arr " + newTechArr);
+
+            // creates the readme files using template literal and data that we have gathered from the user
+            // on line 177, badgeArr[licenseArr.indexOf(response.license)] -> gets the index number of the chosen license from licenseArr and pulls the corresponding badge image from the same index number of badgeArr
             fs.writeFile(`${response.title}.md`, `# ${response.title}
 <br><br>
 
@@ -168,10 +181,12 @@ ${badgeArr[licenseArr.indexOf(response.license)]}
 
 ${apiData.body}
 
-`, (err) =>
+
+`, (err) => // if there is an error, error will be outputed, but if the program goes through everything smoothly, it will log "Created README!"
                 err ? console.error(err) : console.log("Created README!")
 
             );
         }
+        // calls the function for generating the readme
         genReadme();
     })
